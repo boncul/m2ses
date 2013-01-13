c86f72e32e54db13811ce8d07352f835e6a70db7
Ali Burak ÖNCÜL <burak.oncul@bil.omu.edu.tr>
2013-01-13 13:44:01 -0800

    Ses çalma (Pygst) kodu eklendi.

---
+++ ses_cal.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import time
+def cal(veri):
+    m=hecele(veri) 
+    for i in m:
+                
+        s = i + ".mp3"
+        #x=i + ".mp3"
+        import pygst
+        pygst.require("0.10")
+        import gst
+        player = gst.Pipeline("player")
+        source = gst.element_factory_make("filesrc", "file-source")
+        decoder = gst.element_factory_make("mad", "decoder") #mp3 formatı için decoder "mad", örneğin flac için "flacdec"...
+        conv = gst.element_factory_make("audioconvert", "converter")
+        sink = gst.element_factory_make("autoaudiosink", "audio-output") #autoaudiosink yerine sistemin kullandığı ses aygıtı,   			örneğin "alsasink", "alsa-output" yazılabilir
+        player.add(source, decoder, conv, sink)
+        gst.element_link_many(source, decoder, conv, sink)
+        player.get_by_name("file-source").set_property("location", s ) #aynı dizinde değilse tam yol yazılmalı
+        player.set_state(gst.STATE_PLAYING)
+        time.sleep(0.5)
+        #raw_input()
